37986c3ae5b576e23262f2f34ebb7f4753aecef8
Santiacosta74 <santiagomagin.acosta@gmail.com>
2025-08-29 07:39:28 -0300

    Player, Animations and Camera
    -------------------Scripts:
    -Player
    -PlayerAnimator
    -------------------Scene:
    -Player
    -Cinemachine
    -Animator
    -Animations
    -Global Volume edits

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+
+    private bool isWalking;
+
+    private void Update()
+    {
+        Vector2 inputvector = new Vector2 (0,0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputvector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputvector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputvector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputvector.x = +1;
+        }
+
+
+        inputvector = inputvector.normalized;
+
+        Vector3 moveDir = new Vector3(inputvector.x, 0f, inputvector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
